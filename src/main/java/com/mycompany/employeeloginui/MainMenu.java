8347887ea49c01fdccee705f05b5d28c6f5c2f3c
13seachelles <rachelleambayan@gmail.com>
2024-05-15 20:16:19 +0800

    change

--- src/main/java/com/mycompany/employeeloginui/MainMenu.java
+++ src/main/java/com/mycompany/employeeloginui/MainMenu.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package mainmenuinterface;
+package com.mycompany.employeeloginui;;
 
 import javax.swing.*;
 import java.awt.BorderLayout;
