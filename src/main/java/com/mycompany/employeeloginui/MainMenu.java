ec497dd3613c91d12404b1dd5bcb11660512fbdd
13seachelles <rachelleambayan@gmail.com>
2024-05-17 01:50:41 +0800

    changed bg img, button size, and added logout function

--- src/main/java/com/mycompany/employeeloginui/MainMenu.java
+++ src/main/java/com/mycompany/employeeloginui/MainMenu.java
@@ -6,90 +6,96 @@ package com.mycompany.employeeloginui;
 
 
 import javax.swing.*;
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Dimension;
 import javax.swing.JButton;
-import javax.swing.border.BevelBorder;
-import javax.swing.BorderFactory;
-import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-
-
 public class MainMenu {
-    private JFrame Acc;
-    private ImageIcon BgColor;
-    private JLabel label1;
+    private JFrame Acc = new JFrame("Employee Management System");
+    private JLabel label1, label2;
     private JPanel panel1;
-  
     private JButton BTN1,BTN2,BTN3,BTN4;
+    
     MainMenu(){
         //frame
-        Acc = new JFrame();
-        Acc.setTitle("MAIN MENU");
-        
-        Acc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Acc.setResizable(false);
         Acc.setSize(900, 600);
-        
-        Acc.setLayout(new BorderLayout());
-        
-        Acc.getContentPane().setBackground(new Color(204,204,255));
-        
+        Acc.setLayout(null);
+        Acc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
           //label
-          label1 = new JLabel("Employee Management System");
-               
-          label1.setFont(new Font("Courier New", Font.BOLD,40));
-          
-          label1.setHorizontalAlignment(JLabel.CENTER);
-          
-          label1.setBorder(BorderFactory.createEmptyBorder(20,0,20,0));
-          
-          Acc.add(label1, BorderLayout.NORTH);
-          
-          
-         //panel at the bg of the icnos
-        // panel1 = new JPanel();
-         //panel1.setBackground(new Color(204,204,255));
-        //panel1.setBounds(250,250,200,250);
-          //Acc.add(panel1, BorderLayout.CENTER);
-          
-         JPanel panel = new JPanel();
-         panel.setBackground(new Color(204,204,255));
-        panel.setBounds(250,250,200,250);
-          //Acc.add(panel1, BorderLayout.CENTER);
-      panel.setLayout(new GridLayout(7, 2)); 
-
+        label1 = new JLabel("MAIN MENU");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("Arial Rounded MT Bold", Font.BOLD,60));
+        label1.setBounds(270, 55, 500, 100);
+       
         // Create four buttons
         
-       JButton BTN1 = new JButton("Employee List");
-       BTN1.setBounds(10,10,30,50);
-       panel.add(BTN1);
+        BTN1 = new JButton("Employee List");
+        BTN1.setBounds(300,180,300,50);
+        BTN1.setBackground(Color.WHITE);
+        BTN1.setFont(new Font("Arial", Font.BOLD,20));
         
-        JButton BTN2 = new JButton("Employee Dashboard");
+        BTN2 = new JButton("Employee Dashboard");
+        BTN2.setBounds(300,260,300,50);
+        BTN2.setBackground(Color.WHITE);
+        BTN2.setFont(new Font("Arial", Font.BOLD,20));
+        BTN2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Acc.dispose();
+                new EmployeeList();
+                
+            }
+        });
         
-        JButton BTN4 = new JButton("Option");
-       BTN1.setBounds(10,10,100,100);
-       panel.add(BTN4);
+        BTN3 = new JButton("Report and Analytics");
+        BTN3.setBounds(300,340,300,50);
+        BTN3.setBackground(Color.WHITE);
+        BTN3.setFont(new Font("Arial", Font.BOLD,20));
+        
+        BTN3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Acc.dispose();
+                new ReportandAnalyticsUI();
+                
+            }
+        });
+        
+        BTN4 = new JButton("Log out");
+        BTN4.setBounds(700,500,150,50);
+        BTN4.setBackground(Color.WHITE);
+        BTN4.setFont(new Font("Arial", Font.BOLD,20));
+        BTN4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int response = JOptionPane.showConfirmDialog(Acc, "Are you sure you want to logout?", "Logout", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                if (response == JOptionPane.YES_OPTION) {
+                    Acc.dispose();
+                    }
+                }
+            });
+        
+        ImageIcon img = new ImageIcon("Images/bg.jpg");
+        Image img1 = img.getImage().getScaledInstance(950, 600, Image.SCALE_SMOOTH);
+        ImageIcon img2 = new ImageIcon(img1);
+
+        label2 = new JLabel (img2);
+        
+        panel1 = new JPanel();
+        panel1.setBounds (0,-10,900,600);
+        panel1.setBackground(Color.BLACK);
+        panel1.add(label2);
+                
+        Acc.add(label1);
+        Acc.add(BTN1);
+        Acc.add(BTN2);
+        Acc.add(BTN3);
+        Acc.add(BTN4);
+        Acc.add(panel1);
+        
        
-        panel.add(BTN1);
-        panel.add(BTN2);
-        panel.add(BTN3);
-        panel.add(BTN4);
-        
-       
-        
-        Acc.add(panel);
         Acc.setVisible(true);
-        Acc.setLayout(null);
-        
-           
-           
         
     }
     
