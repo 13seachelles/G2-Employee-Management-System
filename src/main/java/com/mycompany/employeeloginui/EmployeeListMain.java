4762e9e26d6d3b9fb5d816ea5a0c400746d6f851
Matthew Tuazon <mtthwtzn@gmail.com>
2024-05-16 22:50:37 +0800

    Added the classes EmployeeList and EmployeeListMain - Tuazon

---
+++ src/main/java/com/mycompany/employeeloginui/EmployeeListMain.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.employeeloginui;
+
+
+public class EmployeeListMain {
+    
+    public static void main (String[] args){
+        
+    new EmployeeList();    
+    
+    }
+    
+}
