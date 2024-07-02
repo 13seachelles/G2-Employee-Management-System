f703a1cd04ad0a679ac435e89955a15bc0641b82
mtthw <mtthw@Matsu>
2024-07-02 14:41:14 +0800

    Connected to SQL and added SQL query to view data from Database

--- src/main/java/com/mycompany/employeeloginui/EmployeeList.java
+++ src/main/java/com/mycompany/employeeloginui/EmployeeList.java
@@ -1,5 +1,6 @@
 package main.java.com.mycompany.employeeloginui;
 
+import java.sql.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -49,31 +50,64 @@ public class EmployeeList extends JFrame {
         logoLabel.setBounds(20, 5, 120, 90);
         backgroundLabel.add(logoLabel);
         
+        JButton viewButton = new JButton("<html>View All Employee<br>Information</html>");
+        viewButton.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
+        viewButton.setBounds(620, 470, 160, 45);
+        backgroundLabel.add(viewButton);
+        
         JButton backButton = new JButton("Back");
         backButton.setFont(new Font("Bookman Old Style", Font.BOLD, 12));
-        backButton.setBounds(700,470,80,45);
+        backButton.setBounds(530,470,80,45);
         backgroundLabel.add(backButton);
         
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {  
-            Frame.dispose();
-            new MainMenu();    
+                Frame.dispose();
+                new MainMenu();    
             }
         });
+        
+        viewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Frame.dispose();
+                new Listtt(); 
+            }
+        });
+        
+         try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_oop", "root", "root");
+            Statement st = conn.createStatement();
+            String query = "SELECT * FROM tbl_employee";
+            ResultSet rs = st.executeQuery(query);
+           
+            while(rs.next()){
+                String employeeID = rs.getString("EmployeeID");
+                String name = rs.getString("Name");
+                String position = rs.getString("Position");
+                String department = rs.getString("Department");
+               
+                addEmployee(employeeID, name, position, department);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while connecting to the database");
+        }
           
-        // Adding sample employee records
-        addEmployee("John K. Makulit", "Tech Specialist", "IT");
-        addEmployee("John Doe", "Designer", "Operations");
+
         
         Frame.setVisible(true);
         Frame.setResizable(false);
     }
 
-    private void addEmployee(String name, String position, String department) {
+    private void addEmployee(String employeeID, String name, String position, String department) {
         JPanel employeePanel = new JPanel();
         employeePanel.setLayout(new BorderLayout());
         employeePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        
+        JLabel employeeIDLabel = new JLabel("Employee ID: " + employeeID);
+        employeeIDLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        employeeIDLabel.setForeground(Color.BLACK);
         
         JLabel nameLabel = new JLabel("Employee Name: " + name);
         nameLabel.setFont(new Font("Arial", Font.BOLD, 18));
@@ -87,26 +121,19 @@ public class EmployeeList extends JFrame {
         departmentLabel.setFont(new Font("Arial", Font.BOLD, 18));
         departmentLabel.setForeground(Color.BLACK);
         
-        JButton viewButton = new JButton("View Information");
-        viewButton.setFont(new Font("Arial", Font.BOLD, 12));
+       
         
-        // TBD, would probs change JOptionPane to a new frame soon instead of a dialog box
-        viewButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(Frame, "Job Description for: " + name ); 
-            }
-        });      
         
         JPanel textPanel = new JPanel();
-        textPanel.setLayout(new GridLayout(3, 1));
-     
+        textPanel.setLayout(new GridLayout(4, 1));
+        
+        textPanel.add(employeeIDLabel);
         textPanel.add(nameLabel);
         textPanel.add(positionLabel);
         textPanel.add(departmentLabel);
         
         employeePanel.add(textPanel, BorderLayout.WEST);
-        employeePanel.add(viewButton, BorderLayout.EAST);
+       
            
         employeeContainer.add(employeePanel);
         employeeContainer.revalidate();
